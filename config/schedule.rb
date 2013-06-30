71362c5e3018cdf86c67a17d8aa33f0a643196b3
Seiji Toyama <seijit@me.com>
2013-06-30 20:12:23 +0900

    Disabled bot

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,8 +4,8 @@ set :environment, :production
 LOG_FOLDER="/srv/datahub/shared/log"
 set :output, {:error => "#{LOG_FOLDER}/cron_error.log", :standard => "#{LOG_FOLDER}/cron.log"}
 
-every 1.hours, :roles => [:db] do
-  %w(bami2).each do |name|
-    rake "run[#{name}]"
-  end
-end
+# every 1.hours, :roles => [:db] do
+#   %w(bami2).each do |name|
+#     rake "run[#{name}]"
+#   end
+# end
