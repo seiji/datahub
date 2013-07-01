ff8c79054aa17271972cadad70fc6c7cfabb88b8
Seiji Toyama <seijit@me.com>
2013-07-01 10:41:50 +0900

    Run script

--- scripts/bami2.rb
+++ scripts/bami2.rb
@@ -4,14 +4,12 @@ $:.unshift 'lib'
 
 require "datahub/bot"
 require "datahub/plugins/twitter/timeline/orb_alert"
-require "datahub/plugins/twitter/timeline/egashura_250"
 
 bot = DataHub::Bot.new do
   configure do |c|
     c.data_source = :mongo
     c.plugins = [
                  DataHub::Plugins::Twitter::Timeline::OrbAlert,
-                 DataHub::Plugins::Twitter::Timeline::Egashura250,
                 ]
   end
 end
