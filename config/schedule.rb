b796918de6c15baf1a36a7f7397b77bae0994f0c
Seiji Toyama <seijit@me.com>
2013-07-04 13:40:15 +0900

    Set schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,8 +4,14 @@ set :environment, :production
 LOG_FOLDER="/srv/datahub/shared/log"
 set :output, {:error => "#{LOG_FOLDER}/cron_error.log", :standard => "#{LOG_FOLDER}/cron.log"}
 
-every 1.hours, :roles => [:db] do
+every '0 * * * *', :roles => [:db] do
   %w(bami2).each do |name|
+    rake "run[#{name}]"
+  end
+end
+
+every '30 * * * *', :roles => [:db] do
+  %w(seijit).each do |name|
     rake "run[#{name}]"
   end
 end
