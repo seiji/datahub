550936110f77c328da294d168ede13bda87ddf24
Seiji Toyama <seijit@me.com>
2013-07-15 10:24:06 +0900

    Check read messages.

--- lib/datahub/plugins/twitter/tag.rb
+++ lib/datahub/plugins/twitter/tag.rb
@@ -11,18 +11,16 @@ module DataHub::Plugins
       end
 
       def execute(coll)
-        require "pp"
         @attributes[:names].each do |name|
           ::Twitter.search("#{name} -rt", :lang => "ja").results.each do |tweet|
             id = tweet[:id]
             user = tweet[:user]
-            pp tweet
-            # if coll.find('_id' => id).count == 0
-            #   coll.insert({_id: id})
-            DataHub::Helpers::write_pubsub_message(@attributes[:pubsub_to],
-                                                   "[#{user[:name]} #{tweet[:text]}"
+             if coll.find('_id' => id).count == 0
+               coll.insert({_id: id})
+               DataHub::Helpers::write_pubsub_message(@attributes[:pubsub_to],
+                                                   "[#{user[:name]}] #{tweet[:text]}"
                                                    )
-            # end
+             end
           end
         end
       end
