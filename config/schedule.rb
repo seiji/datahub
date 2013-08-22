64e907ce08215a73b4c22fdcbafb6e78521c8357
Seiji Toyama <seijit@me.com>
2013-08-22 14:28:24 +0900

    change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :environment, :production
 LOG_FOLDER="/srv/datahub/shared/log"
 set :output, {:error => "#{LOG_FOLDER}/cron_error.log", :standard => "#{LOG_FOLDER}/cron.log"}
 
-every '0 * * * *', :roles => [:db] do
+every '0,30 * * * *', :roles => [:db] do
   %w(bami2).each do |name|
     rake "run[#{name}]"
   end
